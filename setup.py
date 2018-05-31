24218df37a02c1bffb7046c59dbcd0882fb5b992
Victor Fernandez <victor.j.fdez@gmail.com>
2018-05-30 18:04:44 -0600

    added readme to description in pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+	return f.read()
+
 setup(name='chistes_gallegos',
-      version='0.2',
+      version='0.3',
       description='los mejores chistes gallegos',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
       url='https://github.com/victor-fdez/chistes-gallegos',
       author='victor-fdez',
       author_email='victor.j.fdez@gmail.com',
